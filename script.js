f409485bfbfb0e5ab9da1533505dee2b543e39e1
navya1009 <navyagupta1820@gmail.com>
2025-10-05 12:19:08 +0530

    My first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const colorDisplay = document.getElementById("colorDisplay");
+
+function getRandomColor(){
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeColor(){
+    const newColor = getRandomColor();
+    document.body.style.backgroundColor = newColor;
+    colorDisplay.textContent = newColor;
+}
